44de4bde637ab5d1d39c29d6c42c87e6efcfe2e5
The Big J <1710223>
2022-12-16 09:22:14 -0800

    Frog and Fade done

--- Assets/Jonathan/Reset.cs
+++ Assets/Jonathan/Reset.cs
@@ -5,20 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class Reset : MonoBehaviour
 {
-    public FadeScreen fadeScreen;
 
     public void OnTriggerEnter(Collider col)
     {
-        if(col.CompareTag("Player"))
-        {
-            StartCoroutine(ResetScene());
-        }
-    }
-
-    IEnumerator ResetScene()
-    {
-        fadeScreen.FadeOut();
-        yield return new WaitForSeconds(fadeScreen.fadeDuration);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
+
 }
