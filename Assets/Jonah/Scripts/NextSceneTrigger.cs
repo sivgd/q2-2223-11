74011a9d13c85444a6c6dc4601742c7e92f1c5f5
The Big J <1710223>
2023-01-09 09:11:31 -0800

    Looking good

--- Assets/Jonah/Scripts/NextSceneTrigger.cs
+++ Assets/Jonah/Scripts/NextSceneTrigger.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DialogNextSentace : MonoBehaviour
+public class NextSceneTrigger : MonoBehaviour
 {
-    public GameObject Dialog;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +12,6 @@ public class DialogNextSentace : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        FindObjectOfType<DialogManager>().DisplayNextSentence();
-        Dialog.SetActive(false);
+        FindObjectOfType<SceneSwitch>().OnButtonPress4();
     }
 }
